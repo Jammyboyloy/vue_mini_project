c260218a23885fc1c4d0ff3c0d707193229b5bf7
Jammyboyloy <jammylay07@gmail.com>
2026-02-26 21:35:22 +0700

    New

--- src/stores/product.js
+++ src/stores/product.js
@@ -5,10 +5,10 @@ export const useProductStore = defineStore("product", () => {
   let productList = ref([]);
   let productDetail = ref({});
   let loading = ref(false);
-  async function fetchProduct() {
+  async function fetchProduct(item) {
     loading.value = true;
     try {
-      const res = await api.get("/api/products?page=1&per_page=100&search=");
+      const res = await api.get(`/api/products?page=1&per_page=100&search=${item}`);
       // console.log(res);
       productList.value = res.data.data;
     } catch (err) {
